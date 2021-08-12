1823bfdf7be9725b79ab56738c41880692edf63d
hqt2005007 <hqt2005007@gmail.com>
2021-08-12 20:32:13 +0700

    SQL-TEST2

---
+++ 10.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS countries ( 
+country_id varchar(2) IS NOT NULL UNIQUE PRIMARY KEY,
+country_name varchar(40) IS NOT NULL,
+region_id decimal(10,0) IS NOT NULL
+);
--- 6.sql
+++ 6.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS jobs ( 
-job_id varchar(10) NOT NULL , 
-job_title varchar(35) NOT NULL, 
+job_id varchar(10) IS NOT NULL , 
+job_title varchar(35) IS NOT NULL, 
 min_salary decimal(6,0), 
 max_salary decimal(6,0) 
 CHECK(max_salary<=25000)
