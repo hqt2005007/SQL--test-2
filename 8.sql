1060a428da13503e102b96b2f60ccc8a07d54189
hqt2005007 <hqt2005007@gmail.com>
2021-08-12 21:25:08 +0700

    SQL-TEST2

--- 8.sql
+++ 8.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS countries ( 
-country_id varchar(2) IS NOT NULL,
-country_name varchar(40) IS NOT NULL,
-region_id decimal(10,0) IS NOT NULL,
+country_id varchar(2)  NOT NULL,
+country_name varchar(40)  NOT NULL,
+region_id decimal(10,0)  NOT NULL,
 UNIQUE(country_id)
 );
